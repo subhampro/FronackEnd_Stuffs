5a392d88e622848ad386b80fe35221862c5c74be
Subham PRO <subhamproofficial+github@gmail.com>
2024-12-09 23:16:56 +0530

    Add server events for guidebook navigation and opening; implement GPS functionality for points

--- guidebook/server/main.lua
+++ guidebook/server/main.lua
@@ -207,3 +207,18 @@ RegisterCommand(Config.Commands.SendHelp, function(source, args)
         TriggerClientEvent('guidebook:openGuidebook', targetId, pageKey)
     end
 end)
+
+-- Additional Events from commands.lua
+RegisterServerEvent('guidebook:openForPlayer')
+AddEventHandler('guidebook:openForPlayer', function(playerId, pageKey)
+    TriggerClientEvent('guidebook:open', playerId, pageKey)
+end)
+
+RegisterServerEvent('guidebook:navigateToPoint')
+AddEventHandler('guidebook:navigateToPoint', function(pointKey)
+    local source = source
+    local point = MySQL.Sync.fetchAll('SELECT * FROM guidebook_points WHERE `key` = @key', {['@key'] = pointKey})[1]
+    if point and point.can_navigate then
+        TriggerClientEvent('guidebook:setGPS', source, json.decode(point.coords))
+    end
+end)
