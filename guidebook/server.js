5d58bdf078de9256609f637ecb6efeef56cf2d57
Subham PRO <subhamproofficial+github@gmail.com>
2024-12-10 23:43:07 +0530

    Enhance guidebook admin interface: implement new category listing styles, add edit functionality for categories, and update API endpoint comment for clarity

--- guidebook/server.js
+++ guidebook/server.js
@@ -20,7 +20,7 @@ app.get('/api/data', async (req, res) => {
     }
 });
 
-// API endpoint to save data.
+// API endpoint to save data
 app.post('/api/data', async (req, res) => {
     try {
         await fs.writeFile('./ui/mockdata.json', JSON.stringify(req.body, null, 4));
