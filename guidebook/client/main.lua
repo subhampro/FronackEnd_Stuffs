8f3a0c7de4a95b5ca7dc0a016063a557df29c6ca
Subham PRO <subhamproofficial+github@gmail.com>
2024-12-11 14:23:35 +0530

    Refactor guidebook: remove package.json, update client logic for display handling, and enhance UI message handling

--- guidebook/client/main.lua
+++ guidebook/client/main.lua
@@ -1,3 +1,24 @@
+local display = false
 
--- Client logic
-print('Client script loaded')
+RegisterCommand('help', function()
+    SetDisplay(not display)
+end)
+
+RegisterNUICallback('close', function(data, cb)
+    SetDisplay(false)
+    cb('ok')
+end)
+
+function SetDisplay(bool)
+    display = bool
+    SetNuiFocus(bool, bool)
+    SendNUIMessage({
+        type = "ui",
+        status = bool
+    })
+end
+
+CreateThread(function()
+    Wait(1000)
+    TriggerEvent('chat:addSuggestion', '/help', 'Open the guidebook')
+end)
