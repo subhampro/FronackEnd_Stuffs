944829be379fe4364c289cf8c2f9674228c073b7
Subham PRO <subhamproofficial+github@gmail.com>
2024-12-15 08:52:48 +0530

    Add initial React app structure with styling and basic components

--- JS/React/1st_Project/src/index.js
+++ JS/React/1st_Project/src/index.js
@@ -1,8 +1,13 @@
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
+import App from "./App";
 
-// Render your React component instead
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
