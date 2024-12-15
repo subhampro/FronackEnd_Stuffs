b08c74330a9e9e6239309a361bbabf54447cfd26
Subham PRO <subhamproofficial+github@gmail.com>
2024-12-15 23:25:43 +0530

    Fix Map component to include unique keys for list items

--- JS/React/vite_x_react/src/Map.jsx
+++ JS/React/vite_x_react/src/Map.jsx
@@ -3,7 +3,8 @@ export default function Map({colorss}){
     return <>
     <h1>All Colors Function</h1>
     <ul>
-    {colorss.map((color) => <li style={{color: color}}>{color}</li>)}     
+    {/* {colorss.map((color) => <li style={{color: color}}>{color}</li>)}      */}
+    {colorss.map((color, index) => <li key={index} style={{color: color}}>{color}</li>)}     
     </ul>
     </> 
 }
