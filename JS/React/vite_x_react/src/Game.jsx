110ec0787eb8a29a7007998eedd57900b0924a90
Subham PRO <subhamproofficial+github@gmail.com>
2024-12-15 20:40:07 +0530

    Enhance Dice Game component to display winner with color-coded results

--- JS/React/vite_x_react/src/Game.jsx
+++ JS/React/vite_x_react/src/Game.jsx
@@ -1,11 +1,15 @@
 export default function Game(){
     let randNum = Math.floor(Math.random() * 6) + 1;
     let randNum2 = Math.floor(Math.random() * 6) + 1;
+    let styles = {color: randNum === randNum2 ? "green" : "red"}
+
     return <>
     <h1>Dice Game</h1>
+    <div style={styles}>
+    { randNum === randNum2 ? <h1>Player Win</h1> : <h1>Computer Win</h1>  }
     <h2>Player 1 Dice Numver : {randNum} </h2>
     <h2>Player 2 Dice Numver : {randNum2} </h2>
 
-    { randNum === randNum2 ? <h3>Player Win</h3> : <h3>Computer Win</h3>  }
+    </div>
     </>
 }
