3990e4081b493128332500e9e179814ceac192d4
Subham PRO <subhamproofficial+github@gmail.com>
2024-12-16 08:52:12 +0530

    Add Hooks component with counter functionality and associated styles

---
+++ JS/React/react-hooks/src/Hooks.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './Hooks.css';
+
+export default Hooks;
+
+function Hooks(){
+    let [state , setState] = useState(0);
+   return <>
+     <h1>React Hooks</h1> 
+
+       <div>
+          <h2> Count  : {state}</h2>  
+          <button onClick={()=>setState(state => state + 1)}>Functional Update Count</button>
+            <button onClick={()=>setState(state+1)}>Direct Update Count</button>
+            <button onClick={()=> setState(0)}> Reset Counter</button>
+        </div>
+    </>
+}
