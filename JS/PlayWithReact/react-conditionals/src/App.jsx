567a2d5482a78adcf97d1d9a9f52ada02ce204bd
Subham PRO <subhamproofficial+github@gmail.com>
2024-12-17 07:54:49 +0530

    Refactor project structure by adding new Vite configuration files and updating existing ones; remove obsolete files and integrate new components for improved functionality

---
+++ JS/PlayWithReact/react-conditionals/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const count = Math.floor(Math.random() * 6) + 1;
+  const count2 = Math.floor(Math.random() * 6) + 1;
+  let [gameCount, updateGame ]= useState(0);
+  return (
+    <>
+      <h2>Player 1</h2>
+      <h3>Dice : {count}</h3>
+      <h2>Com 1</h2>
+      <h3>Dice : {count2}</h3>
+
+      <h1 style = {count === count2 ?  { color: "green"}:  { color: "red"} }>{count === count2 ? "Player Win" : "Computer Win"}</h1>
+
+      <button onClick={()=>updateGame((Game)=> Game+1)}>Game Count : {gameCount}</button>
+    </>
+  )
+}
+
+export default App
