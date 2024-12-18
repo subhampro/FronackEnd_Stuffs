3e52a1ec80bd2312f2d0e9b45a49735dd9ac747e
Subham PRO <subhamproofficial+github@gmail.com>
2024-12-18 09:31:22 +0530

    Fix RenderArrWidMap component by adding a random key for list items and correcting markup structure

--- JS/PlayWithReact/react-daily-practice-18_12/src/RenderArrWidMap.jsx
+++ JS/PlayWithReact/react-daily-practice-18_12/src/RenderArrWidMap.jsx
@@ -1,13 +1,14 @@
 export default function RenderArrWidMap({arr}) {
+    const num = Math.floor(Math.random()*1000); // Random Number for Key
     return <>
-        <h2>Rendaring Whole Array !
+        <h2>Rendaring Whole Array !</h2>
             So the test array is :   {arr}
 
 <h3>Now The List of array is here !</h3>
             <ul>
-{arr.map((num)=><li>Array Item No. {num}</li>)}
+{arr.map((num)=><li key={num}>Array Item No. {num}</li>)}
 
             </ul>
-        </h2>
+        
     </>
 }
